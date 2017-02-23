0774aead15aa0c1c9c0d4681dbaa5926ce13a857
Isaac Tullis <isaacjtullis@gmail.com>
2017-02-23 14:18:43 -0700

    accessed the current weather for Colorado Springs using fetch

--- public/main.js
+++ public/main.js
@@ -2,3 +2,25 @@ let latitude = 42.35
 let longitude = -71.07
 
 // your code, here
+
+fetch('/api/v1/forecast/38.753762,-104.834571')
+  .then(response=>{
+    if(response.ok){
+      return response;
+    } else {
+      let errorMessage =`${response.status} (${response.statusText})`,
+          error = new Error(errorMessage);
+        throw(error);
+    }
+  })
+  .then(response=>response.json())
+  .then(body=>{
+    var rightNow = body.minutely.summary
+    var temp = body.currently.apparentTemperature
+    var icon = body.currently.icon
+    $('#test').append('<li>'+rightNow+'</li>');
+    $('#test').append('<li>'+'The Current Temperature is: '+temp+' degrees(F).'+'</li>');
+
+  })
+
+  .catch(error=>console.error(`error in fetching data: ${error.message}`));
--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@
 require "sinatra"
 require "net/http"
 
-DARK_SKY_API_KEY = "YOUR-API-KEY-HERE"
+DARK_SKY_API_KEY = "e8fa7d91119176e03264be9d46250122"
 
 get "/api/v1/forecast/:latitude,:longitude" do |latitude, longitude|
   uri = URI("https://api.darksky.net/forecast/#{DARK_SKY_API_KEY}/#{latitude},#{longitude}")
